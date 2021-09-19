f677328b3908f33715834414702f488582f04a26
RyanTheGuyInSpace <ryo_hodge@hotmail.com>
2021-09-19 18:46:56 -0400

    Added gitignore

---
+++ exercise18/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+V
+&src/main/java/exercise/Solution18.java,f\b\fb8e0fe79f81d0b02f4c6c0569b431ef1a560425
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
---
+++ exercise19/src/main/java/exercise/Solution19.java
@@ -0,0 +1,12 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 2 Solution
+ * Copyright 2021 Ryan Hodge
+ */
+
+package exercise;
+
+public class Solution19 {
+    public static void main(String[] args) {
+        
+    }
+}
